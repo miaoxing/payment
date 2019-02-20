94a89a6ad784dfd8f2097feffd7e1c3a08801620
twinh <twinhuang@qq.com>
2019-02-20 11:23:09 +0800

    refactoring: data-table 迁移

--- resources/views/admin/payments/index.php
+++ resources/views/admin/payments/index.php
@@ -59,7 +59,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['dataTable', 'form', 'jquery-deparam'], function () {
+  require(['plugins/admin/js/data-table', 'form', 'jquery-deparam'], function () {
     var recordTable = $('#record-table').dataTable({
       ajax: {
         url: $.url('admin/payments.json')
